86ebfe4c37b05035d80be371043bca6f457015fc
Nattakan Phimmarach <Nattakan.phim@gmail.com>
2024-06-04 18:01:49 +0700

    save

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -6,6 +6,8 @@ function Forecast({ weather }) {
   const { data } = weather;
   const [forecastData, setForecastData] = useState([]);
   const [isCelsius, setIsCelsius] = useState(true); // Track temperature unit
+  const [showWeatherInfo, setShowWeatherInfo] = useState(true);
+  const [showCityName, setShowCityName] = useState(true);
 
   useEffect(() => {
     const fetchForecastData = async () => {
@@ -22,6 +24,15 @@ function Forecast({ weather }) {
 
     fetchForecastData();
   }, [data.city]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setShowWeatherInfo((prevShowWeatherInfo) => !prevShowWeatherInfo);
+      setShowCityName((prevShowCityName) => !prevShowCityName);
+    }, 10000); // 10000 milliseconds = 10 seconds
+
+    return () => clearInterval(interval); // Clean up the interval on unmount
+  }, []);
 
   const formatDay = (dateString) => {
     const options = { weekday: "short" };
@@ -44,10 +55,6 @@ function Forecast({ weather }) {
     setIsCelsius((prevState) => !prevState);
   };
 
-  const convertToCelsius = (temperature) => {
-    return Math.round((temperature - 32) * (5 / 9));
-  };
-
   const convertToFahrenheit = (temperature) => {
     return Math.round((temperature * 9) / 5 + 32);
   };
@@ -61,15 +68,18 @@ function Forecast({ weather }) {
   };
 
   return (
-    <div>
-      <div className="city-name">
-        <h2>
-          {data.city}, <span>{data.country}</span>
-        </h2>
-      </div>
-      <div className="date">
-        <span>{getCurrentDate()}</span>
-      </div>
+    <div className="display">
+      {showCityName ? (
+        <div className="city-name">
+          <h2>
+            {data.city}, <span>{data.country}</span>
+          </h2>
+        </div>
+      ) : (
+        <div className="date">
+          <span>{getCurrentDate()}</span>
+        </div>
+      )}
       <div className="temp">
         {data.condition.icon_url && (
           <img
@@ -82,49 +92,32 @@ function Forecast({ weather }) {
         <sup className="temp-deg" onClick={toggleTemperatureUnit}>
           {isCelsius ? "°C" : "°F"} | {isCelsius ? "°F" : "°C"}
         </sup>
-      </div>
-      <p className="weather-des">{data.condition.description}</p>
-      <div className="weather-info">
-        <div className="col">
-          <ReactAnimatedWeather icon="WIND" size="40"/>
-          <div>
-            <p className="wind">{data.wind.speed}m/s</p>
-            <p>Wind speed</p>
-          </div>
-        </div>
-        <div className="col">
-          <ReactAnimatedWeather icon="RAIN" size="40"/>
-          <div>
-            <p className="humidity">{data.temperature.humidity}%</p>
-            <p>Humidity</p>
-        </div>
-        </div>
-      </div>
-      <>
-      {/* <div className="forecast">
-        <h3>5-Day Forecast:</h3>
-        <div className="forecast-container">
-          {forecastData &&
-            forecastData.slice(0, 5).map((day) => (
-              <div className="day" key={day.time}>
-                <p className="day-name">{formatDay(day.time)}</p>
-                {day.condition.icon_url && (
-                  <img
-                    className="day-icon"
-                    src={day.condition.icon_url}
-                    alt={day.condition.description}
-                  />
-                )}
-                <p className="day-temperature">
-                  {Math.round(day.temperature.minimum)}°/ <span>{Math.round(day.temperature.maximum)}°</span>
-                </p>
+      </div> 
+      {showWeatherInfo ? (
+        <div className="weather-info">
+          <div className="">
+              <div className="col">
+                <ReactAnimatedWeather icon="WIND" size="20" />
+                <div>
+                  <p className="wind">{data.wind.speed}m/s </p>
+                  <p>WindSpeed</p>
+                </div>
               </div>
-            ))}
-        </div>
-      </div> */}
-      </>
+              <div className="col">
+                <ReactAnimatedWeather icon="RAIN" size="20" />
+                <div>
+                  <p className="humidity">{data.temperature.humidity}% </p> 
+                  <p>Humidity</p>
+                </div>
+              </div>
+            </div>
+          </div>
+         
+      ) : (
+        <p className="weather-des">{data.condition.description}</p>
+      )}
     </div>
   );
-}        
+}
 
 export default Forecast;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { StrictMode } from "react";
 
 import App from "./components/App";
-import Credit from "./components/Credit";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
